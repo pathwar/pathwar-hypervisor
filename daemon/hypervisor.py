54a25e635dfd589da0f78ebfe049ec759cba17bf
s. rannou <mxs@sbrk.org>
2015-01-31 17:28:46 +0100

    Now fetching levels from the API. #1

--- daemon/hypervisor.py
+++ daemon/hypervisor.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
+import requests
+import json
+
+
+API_ENDPOINT = 'http://root-token:@212.83.158.125:1337/level-instances?embedded={"level":1}'
+
+
 class Hypervisor:
+
     """
     Hi, I'm the hypervisor:
     - I fetch levels
@@ -9,12 +17,19 @@ class Hypervisor:
     - I sleep
     """
 
-    def __init__(self):
-        print("Hello World")
+    def get_levels(self):
+        r = requests.get(API_ENDPOINT)
+        if r.status_code == 200:
+            return r.json()['_items']
+        return None
 
-    def loop(self):
-        pass
+    def go(self):
+        levels = self.get_levels()
+        if levels:
+            for level in levels:
+                print level
+
 
 if __name__ == '__main__':
     h = Hypervisor()
-    h.loop()
+    h.go()
