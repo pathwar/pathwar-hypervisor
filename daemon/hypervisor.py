0e5cdfa9e614a5a3b4a8230dd876f52c907bab98
s. rannou <mxs@sbrk.org>
2015-01-31 17:42:19 +0100

    Now cloning/pulling levels. #1

--- daemon/hypervisor.py
+++ daemon/hypervisor.py
@@ -2,9 +2,12 @@
 
 import requests
 import json
+import subprocess
+import os.path
 
 
 API_ENDPOINT = 'http://root-token:@212.83.158.125:1337/level-instances?embedded={"level":1}'
+REPO_DIR = 'levels'
 
 
 class Hypervisor:
@@ -17,17 +20,48 @@ class Hypervisor:
     - I sleep
     """
 
+    def __init__(self):
+        """
+        I setup the Hypervisor.
+        """
+        if not os.path.exists(REPO_DIR):
+            os.mkdir(REPO_DIR)
+
     def get_levels(self):
+        """
+        I fetch levels from the API.
+        """
         r = requests.get(API_ENDPOINT)
         if r.status_code == 200:
-            return r.json()['_items']
+            levels = []
+            content = r.json()
+            if '_items' in content:
+                for item in content['_items']:
+                    levels.append(item['level'])
+            return levels
         return None
 
+    def prepare_level(self, level):
+        """
+        I get and prepare the level.
+        """
+        level_dir = '{0}/{1}'.format(REPO_DIR, level['name'])
+        if os.path.exists(level_dir):
+            cmd = 'git pull {0}'.format(level_dir)
+        else:
+            repo = 'git://github.com/pathwar/level-{0}'.format(level['name'])
+            cmd = 'git clone {0} {1}'.format(repo, level_dir)
+        subprocess.call(cmd, shell=True)
+
     def go(self):
+        """
+        I'm the main.
+        """
         levels = self.get_levels()
         if levels:
             for level in levels:
-                print level
+                self.prepare_level(level)
+
 
 
 if __name__ == '__main__':
